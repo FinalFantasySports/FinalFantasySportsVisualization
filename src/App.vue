899fd52143183eb922a937cb0035c8d6798bda2f
Danjenator <danjenator@gmail.com>
2018-10-19 20:05:34 -0500

    we've got a table returning players!

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/football">Football</router-link> |
+      <router-link to="/baseball">Baseball</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
--- src/components/PlayerTable.vue
+++ src/components/PlayerTable.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="ffs-player-table">
-
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          prop="name"
+          label="Name">
+      </el-table-column>
+      <el-table-column
+          prop="position"
+          label="Position">
+      </el-table-column>
+      <el-table-column
+          prop="teamAbbr"
+          label="Team">
+      </el-table-column>
+      <el-table-column
+          prop="fantasyPoints"
+          label="Fantasy Points">
+      </el-table-column>
+      <el-table-column
+          prop="valueBaseDraftScore"
+          label="Value Above Bench">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -9,48 +32,20 @@
 
   export default {
     name: "PlayerTable",
-    data: () => ({
-      columns: [
-        { title: 'Name', field: 'name', sortable: true },
-        { title: 'Position', field: 'position', sortable: true  },
-        { title: 'Team', field: 'teamAbbr', sortable: true  },
-        { title: 'Return Yards', field: 'returnYds', sortable: true },
-        { title: 'Return TD\'s', field: 'returnTds', sortable: true  },
-        { title: 'Fantasy Points', field: 'fantasyPoints', sortable: true  },
-        { title: 'Value Above Replacement', field: 'valueBaseDraftScore', sortable: true  }
-
-      ],
-      data: [],
-      total: 0,
-      query: {}
-    }),
+    data () {
+      return {
+        tableData: []
+      }
+    },
     beforeMount: function() {
       PlayerApi.getAllPlayers()
         .then(res => {
           console.log('res: ', res)
-          this.data = res
+          this.tableData = res
         })
         .catch(err => {
           console.error('Error in Football: ', err)
         })
     }
   }
-
-  // export default {
-  //   name: 'PlayerTable',
-  //   data () {
-  //     return {
-  //       players: null
-  //     }
-  //   },
-  //   mounted () {
-  //     PlayerApi.getAllPlayers()
-  //         .then(res => {
-  //           this.players = res
-  //         })
-  //         .catch(err => {
-  //           console.error('Error in Football: ', err)
-  //         })
-  //   }
-  // }
 </script>
